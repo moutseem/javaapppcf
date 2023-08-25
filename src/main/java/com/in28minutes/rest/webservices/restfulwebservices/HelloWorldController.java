86ef969072eee51ca80d8b83e2fd3733c4bdb0db
mpalegar <mpalegar@deloitte.com>
2023-08-25 15:56:48 +0530

    added code

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/pcfdemo")
+	public String helloWorld() {
+		return "welcome to the PCF Demo";
+	}
+
+}
